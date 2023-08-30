02724f98965eeaa4a9099bc10751bcc4dfd22a1d
adrianosousa <adrianosousa18@outlook.com>
2023-08-30 10:27:39 -0300

    atualizando classe pilha

--- src/estrutura_estatica/Pilha.java
+++ src/estrutura_estatica/Pilha.java
@@ -13,4 +13,21 @@ public class Pilha <T> extends EstruturaEstatica<T>{
     public void empilha(T elemento) {
         super.adiciona(elemento);
     }
+
+    public T topo() {
+        if (this.estaVazia()) {
+            return null;
+        }else {
+            return this.elementos[tamanho-1];
+        }
+    }
+
+    public T desempilha(){
+        if (this.estaVazia()){
+            return null;
+        }
+        T elemento = this.elementos[tamanho-1];
+        tamanho--;
+        return elemento;
+    }
 }
--- src/estrutura_estatica/Teste.java
+++ src/estrutura_estatica/Teste.java
@@ -4,10 +4,30 @@ public class Teste {
     public static void main(String[] args) {
         Pilha<Integer> pilha = new Pilha<Integer>();
 
-        for(int i =0; i<=15; i++){
-            pilha.empilha(i);
-        }
+//        for(int i =0; i<=15; i++){
+//            pilha.empilha(i);
+//         }
+//        System.out.println(pilha);
+//        System.out.println("tamanho da pilha: " + pilha.tamanho());
+
+//        System.out.println(pilha.topo());
+//
+//        System.out.println(pilha);
+
+        pilha.empilha(1);
+        pilha.empilha(2);
+        pilha.empilha(3);
+
         System.out.println(pilha);
-        System.out.println(pilha.tamanho());
+
+        System.out.println("desempilhando elemento: " + pilha.desempilha());
+
+        System.out.println(pilha);
+
+//        System.out.println(pilha.topo());
+//
+//        System.out.println(pilha);
+
+
     }
 }
