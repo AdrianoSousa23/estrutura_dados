524e500630f0ef0502fb416555a76a6b919c2de2
adrianosousa <adrianosousa18@outlook.com>
2023-09-13 10:43:14 -0300

    Lista encadeada

--- lista_encadeada/src/Lista.java
+++ lista_encadeada/src/Lista.java
@@ -7,7 +7,7 @@ public class Lista {
         ListaEncadeada<String> lista = new ListaEncadeada<>();
 
         System.out.println("tamanho da  lista = " + lista.getTamanho());
-
+        //adricionando
 //        lista.adiciona("Claudio");
 //
 //        System.out.println(lista);
@@ -15,17 +15,26 @@ public class Lista {
 //        System.out.println("inicio da lista = " + lista.getInicio().getElemento());
 //        System.out.println("fim da lista = " + lista.getFim().getElemento());
 
+        //adicionando e verificando o elemento na posição
         lista.adiciona("Camila");
         lista.adiciona("adriano");
         lista.adiciona("miguel");
         lista.adiciona("debora");
-
 
         System.out.println(lista);
         System.out.println("tamanho da lista = " + lista.getTamanho());
         System.out.println("inicio da lista = " + lista.getInicio().getElemento());
         System.out.println("fim da lista = " + lista.getFim().getElemento());
 
-        System.out.println("elemento na posição 2 = " + lista.get(2).getElemento());
+        lista.remover("debora");
+        lista.remover("adriano");
+        lista.remover("miguel");
+        System.out.println(lista);
+        System.out.println("tamanho da lista = " + lista.getTamanho());
+        System.out.println("inicio da lista = " + lista.getInicio().getElemento());
+        System.out.println("fim da lista = " + lista.getFim().getElemento());
+
+        // buscando elememto pela posição
+        // System.out.println("elemento na posição 2 = " + lista.get(2).getElemento());
     }
 }
--- lista_encadeada/src/lista/ListaEncadeada.java
+++ lista_encadeada/src/lista/ListaEncadeada.java
@@ -60,4 +60,29 @@ public class ListaEncadeada<Tipo> {
         }
         return atual;
     }
+
+    public void remover (Tipo elemento) {
+        No anterior = null;
+        No atual = this.inicio;
+        for (int i = 0; i < this.getTamanho() ; i++) {
+            if (atual.getElemento().equals(elemento)){
+                if (this.tamanho == 1){
+                    this.inicio = null;
+                    this.fim = null;
+                } else if (atual == inicio) {
+                    this.inicio = atual.getProximo();
+                    atual.setProximo(null);
+                } else if (atual == fim) {
+                    this.fim = anterior;
+                    anterior.setProximo(null);
+                }else
+                anterior.setProximo(atual.getProximo());
+                atual = null;
+                this.tamanho--;
+                break;
+            }
+            anterior = atual;
+            atual = atual.getProximo();
+        }
+    }
 }
